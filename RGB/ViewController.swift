4fb1b9f39980380a17ed8c3420b36d60888ff0fa
Sergey Mikhailov <msv777@land.ru>
2020-04-04 12:06:31 +0300

    Initial Commit

---
+++ RGB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RGB
+//
+//  Created by Sergei Mikhailov on 04.04.2020.
+//  Copyright © 2020 Sergei Mikhailov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
